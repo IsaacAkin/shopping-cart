6c6a9cd2eff7b5650ede0254b6fbed4d974f3eba
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-05-11 21:34:26 +0100

    Populated the Mens component with items fetched from the FakeStoreAPI
    Used fetch to get all items with the category of "men's clothing". Whilst the
    fetch is being carried out, the page displays 'loading...' so the user knows whats
    happneing. If there is an error for any reason, then a message will be shown to the user
    eaplaining that.

--- src/components/Mens.jsx
+++ src/components/Mens.jsx
@@ -1,5 +1,49 @@
 import '../styles/styles.css';
+import { MainContent } from "./utils";
+import { useState, useEffect } from "react";
+// import { useOutletContext } from "react-router";
 
 export default function Mens() {
-  return <h1>Mens Clothing</h1>
+  const [mensClothing, setMensClothing] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // const [cart, setCart] = useOutletContext();
+
+  // function handleCart() {
+  //   setCart(previous => ({...previous, }))
+  // }
+
+  useEffect(() => {
+      async function fetchProducts() {
+          try {
+              const response = await fetch('https://fakestoreapi.com/products');
+              
+              if (!response.ok) {
+                  throw new Error(`Error: ${response.status}`);
+              }
+
+              const data = await response.json();
+              
+              setMensClothing(data.filter(items => items.category === "men's clothing"));
+          } catch (err) {
+              setError(err)
+          } finally {
+              setLoading(false);
+          }
+      }
+
+      fetchProducts();
+  }, []);
+
+  return (
+    <div className='grid-container'>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error loading content</p>}
+      {
+        mensClothing && (
+          <MainContent items={mensClothing} />
+        )
+      }
+    </div>
+  )
 }
