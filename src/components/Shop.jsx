2a8dcb25409092d74b7acfa8b01b6a7e67fcee80
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-05-11 21:28:04 +0100

    Added state to the Shop component for adding items to the cart
    Added state for adding items to the cart. The state will be passed down to all
    child links to the '/shop' route via the Outlets context prop.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,69 +1,18 @@
-// import { useState, useEffect } from 'react';
-// import useFetchProducts from '../hooks/useFetchProducts';
-// import { MainContents } from './utils';
+import { useState } from 'react';
 import { Outlet } from 'react-router';
-import '../styles/styles.css';
 import { Navbar, ShopNavbar } from "./utils";
+import '../styles/styles.css';
 
 export default function Shop() {
-  //   const [products, setProducts] = useState(null);
-  //   const [error, setError] = useState(null);
-  //   const [loading, setLoading] = useState(true);
-
-  //   // const mensClothing = products && products.filter(items => items.category === "men's clothing");
-  //   // const womansClothing = products && products.filter(items => items.category === "women's clothing");
-  //   // const jewelery = products && products.filter(items => items.category === "jewelery");
-  //   // const electronics = products && products.filter(items => items.category === 'electronics');
-
-  //   useEffect(() => {
-  //       async function fetchProducts() {
-  //           try {
-  //               const response = await fetch('https://fakestoreapi.com/products');
-                
-  //               if (!response.ok) {
-  //                   throw new Error(`Error: ${response.status}`);
-  //               }
-
-  //               const data = await response.json();
-                
-  //               setProducts(data.filter(items => items.category === 'electronics'));
-  //           } catch (err) {
-  //               setError(err)
-  //           } finally {
-  //               setLoading(false);
-  //           }
-  //       }
-
-  //       fetchProducts();
-  //   }, []);
-
-  // return (
-  //   <>
-  //     <Navbar />
-  //     {loading && <p>Loading...</p>}
-  //     {error && <p>Error loading content</p>}
-  //     {products && (
-  //       products.map(items => (
-  //         <div key={items.id}>
-  //           {/* <p>{items.image}</p> */}
-  //           <img src={items.image} alt={items.title} />
-  //           <p>{items.title}</p>
-  //           <p>{items.price}</p>
-  //           <p>{items.description}</p>
-  //           <p style={{ fontWeight: 'bold' }}>{items.category}</p>
-  //         </div>
-  //       ))
-  //     )}
-  //   </>
-  // )
+  const [cart, setCart] = useState([]);
 
   return (
-    <div className="shop">
+    <div className="app">
       <Navbar />
-      <div className="wrapper">
+      <div className="shop">
         <ShopNavbar />
         <div className="main-contents" style={{ border: '1px solid red'}}>
-          <Outlet />
+          <Outlet context={[cart, setCart]} />
         </div>
       </div>
     </div>
