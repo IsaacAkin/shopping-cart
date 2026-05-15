a9150ff164519cf1551198a8709cbaaedfbbd454
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-05-15 19:57:14 +0100

    Populated the Cart component with logic for managing the cart
    The carts state is set to the array saved to localStorage, if the array doesn;t exist
    then its set to an empty array instead to signify an empty cart.
    
    the cart saved to local storage is updated each time the cart state changes to keep everything
    up to date.
    
    the increace/decrease and remove item functions are passed down to the Basket component and the
    state is updated on button press.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,43 @@
 import '../styles/styles.css';
+import { Basket } from "./utils";
+import { Navbar } from "./utils";
+import { useEffect, useState } from "react";
 
 export default function Cart() {
-  return <h1>Cart</h1>
+  const [cart, setCart] = useState(() => {
+    const selectedItems = localStorage.getItem('cart');
+    return selectedItems ? JSON.parse(selectedItems) : [];
+  });
+
+  function increaseItemCount(id) {
+    setCart(previous => (previous.map(product => product.id === id ? { ...product, itemCount: product.itemCount + 1 } : product)));
+  }
+  
+  function decreaseItemCount(id) {
+    setCart(previous => (previous.map(product => product.id === id ? { ...product, itemCount: (product.itemCount > 0 ? product.itemCount - 1 : 1) } : product)));
+  }
+
+  function removeItem(id) {
+    setCart(previous => previous.filter(products => products.id !== id));
+  }
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+  return (
+    <div className='app'>
+      <Navbar cartItems={cart.length} />
+      <div className="cart">
+        { cart &&
+          <Basket 
+            products={cart}
+            increase={increaseItemCount}
+            decrease={decreaseItemCount}
+            onClick={removeItem}
+          />
+        }
+      </div>
+    </div>
+  )
 }
