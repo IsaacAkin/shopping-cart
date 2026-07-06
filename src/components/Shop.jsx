0a5672f4adb5ee1903f4214c9289e535445ba5f5
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-07-06 16:46:05 +0100

    Substituted the 'quantity' variable for 'cart.length' to show the amount of items in the cart as it makes more sense

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -9,14 +9,9 @@ export default function Shop() {
     return selectedItems ? JSON.parse(selectedItems) : [];
   });
 
-  let quantity = 0;
-  cart.forEach(item => {
-    quantity += item.itemCount;
-  })
-
   return (
     <div className="app">
-      <Navbar cartItems={quantity}/>
+      <Navbar cartItems={cart.length}/>
       <div className="shop">
         <ShopNavbar />
         <div className="main-contents" style={{ border: '1px solid red'}}>
